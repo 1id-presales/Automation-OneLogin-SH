37a21443ff3c3522fd0d258162930be57f94f1e9
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-08-28 17:51:58 +0100

    Update main.tf

--- Terraform/PreAuth/Block_OneLogin_Portal_CIAM/main.tf
+++ Terraform/PreAuth/Block_OneLogin_Portal_CIAM/main.tf
@@ -1,1 +1,72 @@
-TBC
+/* This Terraform configuration will create a
+For more information please see https://developers.onelogin.com/api-docs/2/smart-hooks/types/pre-authentication */
+
+
+terraform {
+  required_providers {
+    restapi = {
+      source = "Mastercard/restapi"
+      version = "1.18.0"
+    }
+  }
+} 
+
+provider "restapi" {
+  # Configuration options
+   uri                  = "https://${var.ol_subdomain}.onelogin.com/"
+   write_returns_object = true
+   oauth_client_credentials {
+      oauth_client_id = var.ol_client_id
+      oauth_client_secret = var.ol_client_secret
+      oauth_token_endpoint = "https://${var.ol_subdomain}.onelogin.com/auth/oauth2/v2/token"
+  }
+}
+
+########## Terraform VARS ###########
+
+variable "ol_subdomain" {
+  type = string
+  description = "Subdomain name for target OneLogin env"
+}
+
+variable "ol_client_id" {
+  type = string
+  description = "Client ID for API Credential created in target OneLogin env"
+}
+
+variable "ol_client_secret" {
+  type = string
+  description = "Client Secret for API Credential created in target OneLogin env"
+}
+
+############ Var for Smart Hook function ################
+
+variable "ol_smart_hook_function" {
+  type = string
+  description = "function for the pre-auth smart hook"
+  default = <<EOF
+    exports.handler = async (context) => {
+let user = context.user;
+console.log("Context: ", context);
+if (!context.app.id) return { success: false, user: null };
+else {
+		console.log("User not trying to login directly to OneLogin Portal. Doing nothing");
+	}
+  return {
+    success: true,
+    user: {
+      policy_id: context.user.policy_id
+    }
+  }
+}
+    EOF
+}
+
+############ Smart Hook ################
+
+## example of how to create a new pre auth smarthook in your OneLogin environment
+resource "restapi_object" "oneloginsmarthook_pa" {
+  path = "/api/2/hooks"
+  depends_on = [restapi_object.oneloginsmarthook_vars]
+  data = "{ \"type\": \"pre-authentication\", \"disabled\":false, \"runtime\":\"nodejs18.x\", \"context_version\":\"1.1.0\", \"retries\":0, \"timeout\":1, \"options\":{\"location_enabled\":true, \"risk_enabled\":true, \"mfa_device_info_enabled\":true}, \"env_vars\":[], \"packages\": {} , \"function\":\"${base64encode(var.ol_smart_hook_function)}\"}"
+}
