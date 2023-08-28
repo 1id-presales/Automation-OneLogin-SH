8747fc3793ce5fddd3ff8de31394b1b89b52935b
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-08-28 15:40:10 +0100

    Update main.tf

--- Terraform/PreAuth/Block_Country_code/main.tf
+++ Terraform/PreAuth/Block_Country_code/main.tf
@@ -58,7 +58,7 @@ variable "ol_smart_hook_function" {
   description = "function for the pre-auth smart hook"
   default = <<EOF
     exports.handler = async (context) => {
-const blocked_country = process.env.blocked_country_code;
+//const blocked_country = process.env.blocked_country_code;
 console.log("Context: ", context);
 // Deny access
     if (context.location.country_code == "UK") return { success: false, user: null };
