90f9661acba93149a9bd11b3521ef3cd419cec6a
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-08-30 11:19:46 +0100

    Update main.tf

--- Terraform/PreAuth/Switch_Policy_Multi_Auth_Attempts/main.tf
+++ Terraform/PreAuth/Switch_Policy_Multi_Auth_Attempts/main.tf
@@ -68,7 +68,6 @@ variable "ol_smart_hook_function" {
   description = "function for the pre-auth smart hook"
   default = <<EOF
     exports.handler = async (context) => {
-const NewUserPol_ID = process.env.NewUserPol;
 console.log("Context: ", context);
 
   return {
@@ -89,11 +88,20 @@ resource "restapi_object" "oneloginsmarthook_vars" {
   data = "{ \"name\": \"${var.ol_smart_hook_env_var1}\", \"value\": \"${var.ol_policy_id_new_user}\"}"
 }
 
+resource "restapi_object" "oneloginsmarthook_vars2" {
+  path = "/api/2/hooks/envs"
+  data = "{ \"name\": \"redis_host\", \"value\": \"${var.ol_smart_hook_redis_host}\"}"
+}
+
+resource "restapi_object" "oneloginsmarthook_vars3" {
+  path = "/api/2/hooks/envs"
+  data = "{ \"name\": \"redis_pw\", \"value\": \"${var.ol_smart_hook_redis_pw}\"}"
+}
 ############ Smart Hook ################
 
 ## example of how to create a new pre auth smarthook in your OneLogin environment
 resource "restapi_object" "oneloginsmarthook_pa" {
   path = "/api/2/hooks"
   depends_on = [restapi_object.oneloginsmarthook_vars]
-  data = "{ \"type\": \"pre-authentication\", \"disabled\":false, \"runtime\":\"nodejs18.x\", \"context_version\":\"1.1.0\", \"retries\":0, \"timeout\":1, \"options\":{\"location_enabled\":true, \"risk_enabled\":true, \"mfa_device_info_enabled\":true}, \"env_vars\":[\"${var.ol_smart_hook_env_var1}\"], \"packages\": {\"axios\": \"0.21.1\"} , \"function\":\"${base64encode(var.ol_smart_hook_function)}\"}"
+  data = "{ \"type\": \"pre-authentication\", \"disabled\":false, \"runtime\":\"nodejs18.x\", \"context_version\":\"1.1.0\", \"retries\":0, \"timeout\":1, \"options\":{\"location_enabled\":true, \"risk_enabled\":true, \"mfa_device_info_enabled\":true}, \"env_vars\":[\"${var.ol_smart_hook_env_var1}\",\"redis_host\",\"redis_pw\"], \"packages\": {\"axios\": \"0.21.1\"} , \"function\":\"${base64encode(var.ol_smart_hook_function)}\"}"
 }
