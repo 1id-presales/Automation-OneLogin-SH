509b2b3ccf5b21bc69cb6046276d9044691c8582
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-12-22 08:05:15 +0000

    Update main.tf

--- Terraform/UserMigration/Migrate_From_Auth0/main.tf
+++ Terraform/UserMigration/Migrate_From_Auth0/main.tf
@@ -1,5 +1,6 @@
-/* This Terraform configuration will create a 
-For more information please see https://developers.onelogin.com/api-docs/2/smart-hooks/types/ */
+/* This Terraform configuration will create a user migration smart hook in your OneLogin enviornment. This Smart Hook can be used to migrate
+users from an Auth0 environment to OneLogin.
+For more information please see https://developers.onelogin.com/api-docs/2/smart-hooks/types/user-migration */
 
 
 terraform {
@@ -59,11 +60,23 @@ resource "restapi_object" "oneloginsmarthook_vars" {
   data = "{ \"name\": \"${var.ol_smart_hook_env_var1}\", \"value\": \"${var.ol_policy_id_new_user}\"}"
 }
 
+## example of how to create some env vars for Smart Hooks to use
+resource "restapi_object" "oneloginsmarthook_vars2" {
+  path = "/api/2/hooks/envs"
+  data = "{ \"name\": \"${var.ol_smart_hook_env_var2}\", \"value\": \"${var.ol_policy_id_new_user}\"}"
+}
+
+## example of how to create some env vars for Smart Hooks to use
+resource "restapi_object" "oneloginsmarthook_vars3" {
+  path = "/api/2/hooks/envs"
+  data = "{ \"name\": \"${var.ol_smart_hook_env_var3}\", \"value\": \"${var.ol_policy_id_new_user}\"}"
+}
+
 ############ Smart Hook ################
 
 ## example of how to create a new pre auth smarthook in your OneLogin environment
 resource "restapi_object" "oneloginsmarthook_pa" {
   path = "/api/2/hooks"
   depends_on = [restapi_object.oneloginsmarthook_vars]
-  data = "{ \"type\": \"user-migration\", \"disabled\":false, \"runtime\":\"nodejs18.x\", \"context_version\":\"1.1.0\", \"retries\":0, \"timeout\":10, \"options\":{}, \"env_vars\":[\"${var.ol_smart_hook_env_var1}\"], \"packages\": {\"axios\": \"1.1.3\"} , \"function\":\"${base64encode(var.ol_smart_hook_function)}\"}"
+  data = "{ \"type\": \"user-migration\", \"disabled\":false, \"runtime\":\"nodejs18.x\", \"context_version\":\"1.1.0\", \"retries\":0, \"timeout\":10, \"options\":{}, \"env_vars\":[\"${var.ol_smart_hook_env_var1}\",\"${var.ol_smart_hook_env_var2}\",\"${var.ol_smart_hook_env_var3}\"], \"packages\": {\"axios\": \"1.1.3\", \"jwks-rsa\":\"2.0.1\", \"jsonwebtoken\":\"8.5.1\"} , \"function\":\"${base64encode(var.ol_smart_hook_function)}\"}"
 }
